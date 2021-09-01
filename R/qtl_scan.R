d5ee116bc3ba5ef12b950f37d99c48c61ce6f5ce
Karl Broman <kbroman@gmail.com>
2021-09-01 12:35:42 -0500

    drop the slide with just the SNP scan

--- R/qtl_scan.R
+++ R/qtl_scan.R
@@ -25,19 +25,8 @@ ymx <- max(c(ymx_add, ymx_snps))
 
 res <- 255
 
-png(here("Figs/qtl_scan_snps.png"), height=6.5*res, width=11*res, pointsize=14, res=res)
-par(mar=c(2.1,4.1,1.1,1.1))
-layout(c(2,1,2), height=c(0.25, 0.5, 0.25))
 
 endA <- xpos_scan1(pmap, thechr=19, thepos=max(pmap[[19]]))+25/2
-
-plot(out_snps$lod, out_snps$snpinfo, altcol=altcolor, xlab="",
-     ylim=c(0, ymx))
-u <- par("usr")
-segments(u[1], thr_snps$A, endA, thr_snps$A, col=linecolor, lty=2)
-segments(endA, thr_snps$X, u[2], thr_snps$X, col=linecolor, lty=2)
-dev.off()
-
 
 png(here("Figs/qtl_scan.png"), height=6.5*res, width=11*res, pointsize=10, res=res)
 par(mar=c(2.1,4.1,1.1,1.1))
